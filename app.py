c2e0404d2f08fd36c359fe366d68d4f8bc226ac4
112306024-0823 <enotestudy2024@gmail.com>
2024-11-09 21:25:50 +0800

    y

--- app.py
+++ app.py
@@ -1,42 +1,39 @@
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
 
 app = Flask(__name__)
+# LINE BOT info
+line_bot_api = LineBotApi('ZqtE1jWE78Y5oxIkX9d02/E3OdllwiMz5kt+7YF8CBzQqglRuZRulg8dVqoRHpHxcgDwgQXBm8Ld+3rB6VqBFht9nDYkT3CMsE1QamcEAnR7IqHkZTM5VxtFWLpxHL7UJARRKGvttb9MQKV7QXRNuwdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('6362624df36e0b65adbc2a9d2de691eb')
 
-# 初始化 LINE Bot API 和 Webhook Handler
-line_bot_api = LineBotApi(os.getenv('ZqtE1jWE78Y5oxIkX9d02/E3OdllwiMz5kt+7YF8CBzQqglRuZRulg8dVqoRHpHxcgDwgQXBm8Ld+3rB6VqBFht9nDYkT3CMsE1QamcEAnR7IqHkZTM5VxtFWLpxHL7UJARRKGvttb9MQKV7QXRNuwdB04t89/1O/w1cDnyilFU='))
-handler = WebhookHandler(os.getenv('6362624df36e0b65adbc2a9d2de691eb'))
-
-@app.route("/webhook", methods=['POST'])
-def webhook():
-    # 獲取 X-Line-Signature
+@app.route("/callback", methods=['POST'])
+def callback():
     signature = request.headers['X-Line-Signature']
-    # 獲取請求的 JSON 資料
     body = request.get_data(as_text=True)
-
-    # 驗證簽名
+    app.logger.info("Request body: " + body)
+    print(body)
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        abort(400)  # 如果驗證失敗，返回 400 錯誤
-
+        abort(400)
     return 'OK'
 
-# 處理文字訊息事件
-@handler.add(MessageEvent, message=TextMessage)
-def handle_text_message(event):
-    user_message = event.message.text
-    if user_message.lower() == "hello":
-        reply = "Hello! How can I help you today?"
-    else:
-        reply = "I'm here to assist you!"
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply)
-    )
+# Message event
+@handler.add(MessageEvent)
+def handle_message(event):
+    message_type = event.message.type
+    user_id = event.source.user_id
+    reply_token = event.reply_token
+    message = event.message.text
+    line_bot_api.reply_message(reply_token, TextSendMessage(text = "我超愛你"))
 
+import os
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
