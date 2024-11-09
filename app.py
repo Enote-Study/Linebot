17750260bc5a40d0772a9ddd6cfedb74e1e335b3
112306024-0823 <enotestudy2024@gmail.com>
2024-11-09 22:22:46 +0800

    初版_成功部署到render

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def handle_message(event):
     user_id = event.source.user_id
     reply_token = event.reply_token
     message = event.message.text
-    line_bot_api.reply_message(reply_token, TextSendMessage(text = message))
+    line_bot_api.reply_message(reply_token, TextSendMessage(text = "我有收到訊息了哈哈哈哈"))
 
 import os
 if __name__ == "__main__":
